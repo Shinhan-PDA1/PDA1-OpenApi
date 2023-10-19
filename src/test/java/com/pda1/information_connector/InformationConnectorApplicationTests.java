595e6a71acb8d149e6df70397ea6f759fa2e33ca
kolion98 <45055301+kolion98@users.noreply.github.com>
2023-10-19 11:17:22 +0900

    Update file

--- src/test/java/com/pda1/information_connector/InformationConnectorApplicationTests.java
+++ src/test/java/com/pda1/information_connector/InformationConnectorApplicationTests.java
@@ -1,13 +1,13 @@
-package com.pda1.information_connector;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class InformationConnectorApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.pda1.information_connector;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class InformationConnectorApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
